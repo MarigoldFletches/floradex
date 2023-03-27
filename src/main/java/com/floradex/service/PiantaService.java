713145ee3569d94f8d0e46f4d6542ee386d95d3c
MarigoldFletches <chiara.ven.416@gmail.com>
2023-03-27 23:07:51 +0200

    first commit

---
+++ src/main/java/com/floradex/service/PiantaService.java
@@ -0,0 +1,17 @@
+package com.floradex.service;
+
+import java.util.List;
+
+import com.floradex.entity.Pianta;
+
+public interface PiantaService {
+
+	List<Pianta> getAllPiante();
+	Pianta getPiantaById(Integer id);
+	Pianta savePianta(Pianta pianta);
+	Pianta updatePianta(Integer id, Pianta pianta);
+	void deletePiantaById(Integer id);
+	List<Pianta> getPianteByName(String nome);
+	
+
+}
